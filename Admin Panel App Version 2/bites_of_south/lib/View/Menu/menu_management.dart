02b9c0843754c53109749328edd0757190525ffe
DAKSH RATHOD <dakshbarthod@gmail.com>
2025-01-19 19:57:04 +0530

    Firebase run successfully done

---
+++ Admin Panel App Version 2/bites_of_south/lib/View/Menu/menu_management.dart
@@ -0,0 +1,84 @@
+import 'package:bites_of_south/Controller/menu_provider.dart';
+import 'package:bites_of_south/View/Menu/add_item_to_menu.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MenuManagementScreen extends StatefulWidget {
+  @override
+  _MenuManagementScreenState createState() => _MenuManagementScreenState();
+}
+
+class _MenuManagementScreenState extends State<MenuManagementScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Fetch menu items from Firestore when the screen is initialized
+ WidgetsBinding.instance.addPostFrameCallback((_) {
+    Provider.of<MenuProvider>(context, listen: false).fetchMenuItems();
+  });  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Menu Management"),
+      ),
+      body: Consumer<MenuProvider>(
+        builder: (context, menuProvider, child) {
+          if (menuProvider.isLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+            ),
+            itemCount: menuProvider.menuItems.length,
+            itemBuilder: (context, index) {
+              final menuItem = menuProvider.menuItems[index];
+              return GestureDetector(
+                onTap: () {
+                  // Navigate to details screen (not implemented here)
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    image: DecorationImage(
+                      image: NetworkImage(menuItem.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: GridTile(
+                    child: Container(
+                      color: Colors.black.withOpacity(0.5),
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          menuItem.title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                  ),
+                ),
+              ));
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddItemToMenuScreen()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
--- Admin Panel App Version 2/bites_of_south/lib/main.dart
+++ Admin Panel App Version 2/bites_of_south/lib/main.dart
@@ -1,8 +1,23 @@
+import 'package:bites_of_south/Controller/menu_provider.dart';
+import 'package:bites_of_south/View/Dashboard.dart';
+import 'package:bites_of_south/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+ runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MenuProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -31,7 +46,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DashboardScreen(),
     );
   }
 }
