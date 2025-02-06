c47b5f419d622d9f0fd75ecf0745b982f17da791
DESKTOP-AP2JBKD\jeel <jeelsam@gmail.com>
2025-02-06 22:35:31 +0530

    Menu Firebase

---
+++ FinalYearProject-Web/bites-of-south/src/Home.js
@@ -0,0 +1,238 @@
+// src/Home.js
+import React from 'react';
+import './Home.css';  // Import the CSS file
+
+const Home = () => {
+  return (
+    <div className="home">  {/* Apply the CSS class */}
+      <section className="FirstH">
+        <div className="nav">
+          <div className="logo">
+            <span>
+              <img src="/images/dosaimg.png" alt="Logo" loading="lazy" />
+              <h3>BitesOfSouth</h3>
+            </span>
+          </div>
+          <div className="options">
+            <ul className="NavOption">
+              <li className="Bscreen">
+                <a href="">Home</a>
+              </li>
+              <li className="Bscreen">
+                <a href="">Order</a>
+              </li>
+              <li className="Bscreen">
+                <a href="">About Us</a>
+              </li>
+              <li className="signin">
+                <a href="">Sign in</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="SecondH">
+          <div className="LSideImg">
+            <img src="/images/!TEmpSIdePic.avif" alt="" />
+          </div>
+          <div className="SloganSearch">
+            <div className="MidPart">
+              <div className="search-container">
+                <input
+                  type="text"
+                  className="serachH"
+                  placeholder="Search for Dosa, items or more"
+                />
+                <button className="searchHbtn">
+                  <i className="fa-solid fa-magnifying-glass" id="search"></i>
+                </button>
+              </div>
+              <h1 className="slogan">Bites of South: Where Food Meets Culture.</h1>
+
+              <div className="FoodOption">
+                <div className="Option1">
+                  <a href='/menu'> 
+                  <h1>Order</h1>
+                  <h2>Order Food Online</h2>
+                  <img src="/images/Dosa.avif" alt="" />
+                  </a>
+                </div>
+                <div className="Option1">
+                  <h1>Order</h1>
+                  <h2>Order Food Online</h2>
+                  <img src="/images/Dosa.avif" alt="" />
+                </div>
+                <div className="Option1">
+                  <h1>Order</h1>
+                  <h2>Order Food Online</h2>
+                  <img src="/images/Dosa.avif" alt="" />
+                </div>
+                <div className="Option1" id="mobshow">
+                  <h1>Order</h1>
+                  <h2>Order Food Online</h2>
+                  <img src="/images/Dosa.avif" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="RSideImg">
+            <img src="/images/TempPIcsSIde.avif" alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section className="FirstOrder">
+        <div className="carousel-container">
+          <h3 className="carousel-title">Inspiration for your first order</h3>
+          <div className="carousel">
+            <div className="slide">
+              <a className="food-link" href="/mumbai/delivery/dish-pizza">
+                <div className="image-container">
+                  <img
+                    className="food-image"
+                    src="https://b.zmtcdn.com/data/o2_assets/d0bd7c9405ac87f6aa65e31fe55800941632716575.png"
+                    alt="Pizza"
+                  />
+                </div>
+                <p className="food-title">Pizza</p>
+              </a>
+            </div>
+            <div className="slide">
+              <a className="food-link" href="/mumbai/delivery/dish-biryani">
+                <div className="image-container">
+                  <img
+                    className="food-image"
+                    src="https://b.zmtcdn.com/data/o2_assets/bf2d0e73add1c206aeeb9fec762438111727708719.png"
+                    alt="Biryani"
+                  />
+                </div>
+                <p className="food-title">Biryani</p>
+              </a>
+            </div>
+            <div className="slide">
+              <a className="food-link" href="/mumbai/delivery/dish-chicken">
+                <div className="image-container">
+                  <img
+                    className="food-image"
+                    src="https://b.zmtcdn.com/data/dish_images/197987b7ebcd1ee08f8c25ea4e77e20f1634731334.png"
+                    alt="Chicken"
+                  />
+                </div>
+                <p className="food-title">Chicken</p>
+              </a>
+            </div>
+            <div className="slide">
+              <a className="food-link" href="/mumbai/delivery/dish-burger">
+                <div className="image-container">
+                  <img
+                    className="food-image"
+                    src="https://b.zmtcdn.com/data/dish_images/ccb7dc2ba2b054419f805da7f05704471634886169.png"
+                    alt="Burger"
+                  />
+                </div>
+                <p className="food-title">Burger</p>
+              </a>
+            </div>
+            <div className="slide">
+              <a className="food-link" href="/mumbai/delivery/dish-fried-rice">
+                <div className="image-container">
+                  <img
+                    className="food-image"
+                    src="https://b.zmtcdn.com/data/o2_assets/e444ade83eb22360b6ca79e6e777955f1632716661.png"
+                    alt="Fried Rice"
+                  />
+                </div>
+                <p className="food-title">Fried Rice</p>
+              </a>
+            </div>
+            <div className="slide">
+              <a className="food-link" href="/mumbai/delivery/dish-idli">
+                <div className="image-container">
+                  <img
+                    className="food-image"
+                    src="https://b.zmtcdn.com/data/dish_images/d9766dd91cd75416f4f65cf286ca84331634805483.png"
+                    alt="Idli"
+                  />
+                </div>
+                <p className="food-title">Idli</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+<section className="maps">
+    <div className="TitleMaps">
+        <h2>We are located over here!</h2>
+    </div>
+    <div className="googlemapsD"> 
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.764234514681!2d72.847182!3d19.1617948!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b64deb48f999%3A0xaeddd49518fd2974!2sBalaji%20Dosa%20Corner!5e0!3m2!1sen!2sin!4v1738253951373!5m2!1sen!2sin" allowFullScreen loading="lazy"></iframe> 
+    </div>    
+</section>
+
+      <section className="Footer">
+      <div className="footer-container">
+        <div className="footer-main">
+            <div className="footer-logo">
+                <div className="logo-image">
+                    <img src="/images/dosaimg.png" alt="Logo" loading="lazy"/>
+                </div>
+            </div>
+            <div className="footer-text">© 2025 Bites Of South Limited</div>
+        </div>
+
+        <div className="footer-links">
+            <ul className="company-links">
+                <li className="link-item"><div className="section-title">Company</div></li>
+                <li className="link-item"><a href="https://www.swiggy.com/about" target="_blank">About Us</a></li>
+                <li className="link-item"><a href="https://www.swiggy.com/support" target="_blank">Help & Support</a></li>
+                <li className="link-item"><a href="https://partner-with-us.swiggy.com/onboard#/swiggy" target="_blank" rel="nofollow noopener">Partner with Us</a></li>
+                <li className="link-item"><a href="https://ride.swiggy.com" target="_blank" rel="nofollow noopener">Contact US</a></li>
+            </ul>
+
+            <ul className="legal-links">
+                <li className="link-item"><div className="section-title">Legal</div></li>
+                <li className="link-item"><a href="https://www.swiggy.com/terms-and-conditions" target="_blank">Terms & Conditions</a></li>
+                <li className="link-item"><a href="https://www.swiggy.com/cookie-policy" target="_blank">Cookie Policy</a></li>
+                <li className="link-item"><a href="https://www.swiggy.com/privacy-policy" target="_blank">Privacy Policy</a></li>
+            </ul>
+
+            <ul className="location-links">
+                <li className="link-item"><div className="section-title">Available at:</div></li>
+                <li className="link-item"><a href="/city/bangalore">Mumbai</a></li>
+            </ul>
+
+
+                
+                <ul className="payment-methods-list">                       
+                        <li className="link-item"><div className="section-title">Payment Methods</div></li>
+                        <li className="link-item"> <img className="payment-icon" src="https://lapinozpizza.in/assets/wla_new/img/footer/upi-icon.svg" width="50" height="30" alt="UPI" loading="lazy"/> <img className="payment-icon" src="https://lapinozpizza.in/assets/wla_new/img/footer/gpay-svg.svg" width="50" height="30" alt="Google Pay" loading="lazy"/></li>
+                        <li className="link-item"> <img className="payment-icon" src="https://lapinozpizza.in/assets/wla_new/img/footer/paytm-svg.svg" width="50" height="30" alt="Paytm" loading="lazy"/> <img className="payment-icon" src="https://lapinozpizza.in/assets/wla_new/img/footer/visa-svg.svg" width="50" height="30" alt="Visa" loading="lazy"/></li>       
+                </ul>
+
+            
+            <div className="social-links">
+                <a href="https://www.linkedin.com/company/swiggy-in/" target="_blank">
+                    <img src="https://media-assets.swiggy.com/portal/testing/seo-home/Linkedin.svg" alt="LinkedIn" loading="lazy"/>
+                </a>
+                <a href="https://www.instagram.com/swiggyindia/?hl=en" target="_blank">
+                    <img src="https://media-assets.swiggy.com/portal/testing/seo-home/icon-instagram.svg" alt="Instagram" loading="lazy"/>
+                </a>
+                <a href="https://www.facebook.com/swiggy.in/" target="_blank">
+                    <img src="https://media-assets.swiggy.com/portal/testing/seo-home/icon-facebook.svg" alt="Facebook" loading="lazy"/>
+                </a>
+                <a href="https://in.pinterest.com/swiggyindia/" target="_blank">
+                    <img src="https://media-assets.swiggy.com/portal/testing/seo-home/icon-pinterest.svg" alt="Pinterest" loading="lazy"/>
+                </a>
+                <a href="https://twitter.com/Swiggy?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank">
+                    <img src="https://media-assets.swiggy.com/portal/testing/seo-home/Twitter.svg" alt="Twitter" loading="lazy"/>
+                </a>
+            </div>
+        </div>
+    </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
---
+++ FinalYearProject-Web/bites-of-south/src/Menu.js
@@ -0,0 +1,98 @@
+import React, { useEffect } from 'react';
+import './Menu.css'; // If you're using a separate CSS file
+
+import { getDocs, collection } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnSJVHioItNsc2kedyZTxJ7PvfX2hQC7Q",
+  authDomain: "bitesofsouth-a38f4.firebaseapp.com",
+  databaseURL: "https://bitesofsouth-a38f4-default-rtdb.firebaseio.com",
+  projectId: "bitesofsouth-a38f4",
+  storageBucket: "bitesofsouth-a38f4.firebasestorage.app",
+  messagingSenderId: "65231955877",
+  appId: "1:65231955877:web:aab053b6882e9894bdaa4c",
+  measurementId: "G-R9WE265DPN"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+function App() {
+  useEffect(() => {
+    const fetchMenu = async () => {
+      const menuContainer = document.querySelector(".DishItems");
+      const querySnapshot = await getDocs(collection(db, "menu"));
+
+      menuContainer.innerHTML = ""; // Clear previous content
+
+      if (querySnapshot.empty) {
+        menuContainer.innerHTML = "<p>No menu items available.</p>";
+        return;
+      }
+
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        const foodItemHTML = `
+            <div class="DishItmesPlacement">
+                <div class="LSidePlacement">
+                    <div class="ItemType">
+                        <img src="veg-icon.jpeg" alt="">
+                    </div>
+                    <div class="itemTitle">
+                        <h3>${data.title}</h3>
+                    </div>
+                    <div class="itemPrice">
+                        <h4>₹${data.price}</h4>
+                    </div>
+                    <div class="itemRatings">
+                        <img src="/images/Stars.png" alt="">  <p>${data.rating}</p>
+                    </div>
+                    <div class="itemDescription">
+                        <h5>${data.description}</h5>
+                    </div>
+                </div>
+                <div class="RSidePlacement">
+                    <div class="ItemsImgs">
+                        <img src="${data.image}" alt="DosaImg" height="144px" width="156px">
+                    </div>   
+                    <div class="AddToCart">
+                        <button>Add</button>
+                    </div>
+                </div>
+            </div>
+        `;
+        menuContainer.innerHTML += foodItemHTML;
+      });
+    };
+
+    fetchMenu();
+  }, []);
+
+  return (
+    <div>
+      <div className="MTitle">
+        <div className="LArt"></div>
+        <div className="MHead">
+          <h3>⤛  Menu  ⤜</h3>
+        </div>
+        <div className="RArt"></div>
+      </div>
+      <div className="SearchBtn">
+        <input type="text" placeholder="Search for dishes" />
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </div>
+
+      <div className="line"></div>
+
+      <div className="DishItems">
+        {/* Content will be inserted here dynamically */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
